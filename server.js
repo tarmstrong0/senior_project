a359fb734e811e91e5102ab13b1eb19d2bd6c2f6
Brad Harris <bmharris@gmail.com>
2012-04-24 04:12:42 +0000

    cleanup

--- server.js
+++ server.js
@@ -4,16 +4,11 @@
  */
 
 var express = require('express'),
-	routes = require('./routes'),
-	swig = require('swig');
-
-var app = module.exports = express.createServer();
+	swig = require('swig'),
+    app = module.exports = express.createServer();
 
 // Configuration
-
 app.configure(function(){
-
-
 	swig.init({
 		root: __dirname + '/views',
 		allowErrors: true
@@ -25,7 +20,6 @@ app.configure(function(){
 		layout: false,
 	});
 	app.set('view cache', false);
-
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(app.router);
@@ -41,9 +35,9 @@ app.configure('production', function(){
 });
 
 // Routes
-
-app.get('/', routes.index);
-app.get('/search/', routes.search);
+var controller = require('./controllers/index.js');
+app.get('/', controller.index);
+app.get('/search/', controller.search);
 
 app.listen(process.env.C9_PORT);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
